fd25cd3a0d1e207f674e3c6b07335cc313ddaa24
Ryan Ayton <ryanianhamlet@gmail.com>
2021-04-24 17:28:32 +0100

    Added interactable terminal

---
+++ Assets/Scripts/Character/Character.cs
@@ -0,0 +1,40 @@
+using System;
+using DG.Tweening;
+using UnityEngine;
+
+
+public class Character : MonoBehaviour
+{
+    private CharacterMovement _characterMovement;
+    private CharacterRotation _characterRotation;
+    private Rigidbody2D _rb;
+    void Start()
+    {
+        _characterMovement = GetComponent<CharacterMovement>();
+        _characterRotation = GetComponent<CharacterRotation>();
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void OverrideMovement()
+    {
+        _characterMovement.enabled = false;
+        _characterRotation.enabled = false;
+        _rb.isKinematic = true;
+    }
+
+    public void ForceToPosition(Vector2 pos, float angle, Action onComplete,float duration = 0.5f)
+    {
+        OverrideMovement();
+        _rb.velocity = Vector2.zero;
+        _rb.angularVelocity = 0;
+        _rb.DORotate(angle, duration);
+        _rb.DOMove(pos,duration).onComplete = () => onComplete();
+    }
+
+    public void RegainControl()
+    {
+        _characterMovement.enabled = true;
+        _characterRotation.enabled = true;
+        _rb.isKinematic = false;
+    }
+}
---
+++ Assets/Scripts/Interaction/Terminal.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Terminal : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private Sprite _activeSprite;
+    [SerializeField] private Sprite _interactableSprite;
+    [SerializeField] private Sprite _inActiveSprite;
+    [SerializeField] private float interactionTime;
+    [SerializeField] private Transform interactionPoint;
+
+    [SerializeField] private InteractionBar _interactionBar;
+    
+    private Character _characterInRange;
+    private bool _used;
+
+    public void Start()
+    {
+        _interactionBar.gameObject.SetActive(false);
+    }
+    
+    public void Interact()
+    {
+        StartCoroutine(InteractCo());
+    }
+
+    public IEnumerator InteractCo()
+    {
+        _interactionBar.gameObject.SetActive(true);
+        _interactionBar._fillImage.fillAmount = 0;
+        _interactionBar._fillImage.DOFillAmount(1f,interactionTime).SetEase(Ease.Linear);
+        yield return new WaitForSeconds(interactionTime);
+        _interactionBar.gameObject.SetActive(false);
+        _used = true;
+        _spriteRenderer.sprite = _inActiveSprite;
+        _characterInRange.RegainControl();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (_used)
+        {
+            return;
+        }
+
+        var character = other.GetComponent<Character>();
+        if(character != null)
+        {
+            _characterInRange = character;
+            _spriteRenderer.sprite = _interactableSprite;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (_used)
+        {
+            return;
+        }
+        var character = other.GetComponent<Character>();
+        if(character != null)
+        {
+            _characterInRange = null;
+            _spriteRenderer.sprite = _activeSprite;
+        }
+    }
+    
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (_used)
+        {
+            return;
+        }
+
+        if (_characterInRange != null)
+        {
+            _characterInRange.ForceToPosition(interactionPoint.position,Vector2.Angle(interactionPoint.position,transform.position) + 90,Interact);
+        }
+    }
+}
