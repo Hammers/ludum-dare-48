c5f04c24943816182d2cc5f6f9a9f2e987474f9f
Max <m.finchbretlaender@gmail.com>
2021-04-24 12:12:10 +0100

    Added a camera follow script to the main camera.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform target;
+    private MapGenerator mapGenerator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mapGenerator = GameObject.Find("Grid").GetComponent<MapGenerator>();
+        target = GameObject.Find("PlayerCharacter").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+}
